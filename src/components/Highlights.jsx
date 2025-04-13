75bfa2c0757907a0a1ee1cc4ea1660d404e8a443
a5hcodes <rajana22cs@student.mes.ac.in>
2025-04-13 12:42:39 +0530

    vid carousel fixed

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -8,7 +8,7 @@ const Highlights = () => {
 
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0 })
-    gsap.to('.link', { opacity: 1, y: 0 , duration:1, stagger:0.25}) // for one by one animation
+    gsap.to('.link', { opacity: 1, y: 0 , duration:1, stagger:0.25}) // stragger for one by one animation
   }, [])
   return (
     <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
